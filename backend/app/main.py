0768d66936f9f9469d46e5b4baf7299b4d9d0b96
Marcus Brandão <marcusergue@gmail.com>
2026-04-27 09:29:27 -0300

    backend: configura conexao inicial com o banco de dados

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,21 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from app.core.config import settings
+from app.core.database import create_db_and_tables
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
 
 app = FastAPI(
     title=settings.APP_NAME,
     description=settings.APP_DESCRIPTION,
     version=settings.APP_VERSION,
+    lifespan=lifespan,
 )
 
 
